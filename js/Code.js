b08d859261b1ce799efebc3070012202ae4b7ee3
Srishti-Sudesh <76618010+Srishti-Sudesh@users.noreply.github.com>
2021-05-12 16:38:16 +0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant!", 100,70)
+   
+    fill("white")
+    textSize(15)
+    text("U F I O T N C N", 650,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task!", 650,170)
+
+    fill("white")
+    textSize(15)
+    text("A T E B E D S A A", 100,280)
+    fill("lightblue")
+    text("Hint: Stores all information!", 100,300)   
+  
+    fill("white")
+    textSize(15);
+    text("Note: Type your answers in ALL CAPS", 700,470);
+}
